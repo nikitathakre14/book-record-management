c667c3edc33a458ff47eda96b383a8c9d3c61b6c
nikitathakre14 <nikitathakre117@gmail.com>
2022-12-11 08:57:01 +0530

    added routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+//importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
+const app = express();
+const PORT = 8081;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Server is up and running",
+    });
+});
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
+app.get("*", (req, res) => {
+    res.status(404).json({
+        message: "This route does not exist",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
